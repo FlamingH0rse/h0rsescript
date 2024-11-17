1daaaa1217a3af27a0177a9b43b0afcc3144def1
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-17 22:27:57 +0530

    Fix

--- src/nativeMain/kotlin/h0rsescript/error/InvalidTokenError.kt
+++ src/nativeMain/kotlin/h0rsescript/error/InvalidTokenError.kt
@@ -1,12 +1,10 @@
 package me.flaming.h0rsescript.error
 
-import me.flaming.h0rsescript.Token
-
 class InvalidTokenError(char: Char, line: Int?, column: Int?) : HSError() {
     init {
         super.type = ErrorType.SYNTAX
         super.name = "InvalidTokenError"
-        super.message = "Invalid token '${token.value}'"
+        super.message = "Invalid token '${char}'"
         super.line = line
         super.column = column
     }
