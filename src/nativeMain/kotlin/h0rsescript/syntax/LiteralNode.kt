3981629d1bbc8f41bc229d9df41c4db91f89b9ad
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-17 23:21:51 +0530

    Added value property to LiteralNode

--- src/nativeMain/kotlin/h0rsescript/syntax/LiteralNode.kt
+++ src/nativeMain/kotlin/h0rsescript/syntax/LiteralNode.kt
@@ -2,8 +2,13 @@ package me.flaming.h0rsescript.SyntaxTrees
 
 import me.flaming.h0rsescript.syntax.ASTNode
 
-data class LiteralNode (val value: String, val type: LiteralType): ASTNode {
+data class LiteralNode (val rawValue: String, val type: LiteralType): ASTNode {
     enum class LiteralType {
         STRING, NUMBER, BOOLEAN
+    }
+    val value = when (type) {
+        LiteralType.STRING -> rawValue
+        LiteralType.NUMBER -> rawValue.toDouble()
+        LiteralType.BOOLEAN -> rawValue.lowercase().toBooleanStrict()
     }
 }
