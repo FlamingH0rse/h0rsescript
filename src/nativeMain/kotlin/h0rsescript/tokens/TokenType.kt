46982190df2e16eed12286f17f990b31ac8bb395
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-17 23:06:18 +0530

    Rename SYMBOL type to ASSIGNMENT_OPERATOR for clarity

--- src/nativeMain/kotlin/h0rsescript/tokens/TokenType.kt
+++ src/nativeMain/kotlin/h0rsescript/tokens/TokenType.kt
@@ -3,7 +3,7 @@ package me.flaming.h0rsescript.tokens
 enum class TokenType {
     IDENTIFIER,    // Variable or function names
     KEYWORD,       // Keywords like $def, $end, etc.
-    SYMBOL,        // Symbols like ->, >, <->, etc.
+    ASSIGNMENT_OPERATOR,        // Symbols like ->, <->, etc.
     STRING,        // String literals
     NUMBER,        // Numeric literals
     BOOLEAN,       // true/false/TRUE/FALSE
