d05b99f0e3f18b8358bfc5a4d00292226c889383
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-18 14:08:06 +0530

    Changed return type of ErrorHandler.report to Nothing

--- src/nativeMain/kotlin/h0rsescript/ErrorHandler.kt
+++ src/nativeMain/kotlin/h0rsescript/ErrorHandler.kt
@@ -4,7 +4,7 @@ import me.flaming.h0rsescript.error.HSError
 import kotlin.system.exitProcess
 
 object ErrorHandler {
-    fun report(error: HSError) {
+    fun report(error: HSError): Nothing {
         val message = error.getMessage()
         println(message)
         exitProcess(1)
