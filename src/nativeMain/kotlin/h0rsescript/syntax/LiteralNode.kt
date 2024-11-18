d163e4f15d5794a5f5f992bd79f9cf21dbaa750a
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-19 00:02:01 +0530

    Implemented type parsing for literals

--- src/nativeMain/kotlin/h0rsescript/syntax/LiteralNode.kt
+++ src/nativeMain/kotlin/h0rsescript/syntax/LiteralNode.kt
@@ -3,12 +3,27 @@ package me.flaming.h0rsescript.SyntaxTrees
 import me.flaming.h0rsescript.syntax.ASTNode
 
 data class LiteralNode (val rawValue: String, val type: LiteralType): ASTNode {
+    val escapeSequences = mapOf(
+        "\\\\" to "\\",
+        "\\n" to "\n",
+        "\\t" to "\t",
+        "\\r" to "\r",
+        "\\b" to "\b",
+        "\\\"" to "\""
+    )
     enum class LiteralType {
         STRING, NUMBER, BOOLEAN
     }
     val value = when (type) {
-        LiteralType.STRING -> rawValue
+        LiteralType.STRING -> unescape(rawValue)
         LiteralType.NUMBER -> rawValue.toDouble()
         LiteralType.BOOLEAN -> rawValue.lowercase().toBooleanStrict()
+    }
+    private fun unescape(str: String): String {
+        var unescapedStr = str
+        for ((k,v) in escapeSequences) {
+            unescapedStr = unescapedStr.replace(k, v)
+        }
+        return unescapedStr
     }
 }
