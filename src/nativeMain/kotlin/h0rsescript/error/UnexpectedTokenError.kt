b273f96911bff33286f159a276f3fed948e5618e
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-18 14:08:30 +0530

    Added exact value match option for UnexpectedTokenError

--- src/nativeMain/kotlin/h0rsescript/error/UnexpectedTokenError.kt
+++ src/nativeMain/kotlin/h0rsescript/error/UnexpectedTokenError.kt
@@ -3,11 +3,11 @@ package me.flaming.h0rsescript.error
 import me.flaming.h0rsescript.tokens.Token
 import me.flaming.h0rsescript.tokens.TokenType
 
-class UnexpectedTokenError(token: Token?, vararg expectedTypes: TokenType) : HSError() {
+class UnexpectedTokenError(token: Token?, vararg expectedTypes: TokenType, expectedValue: String? = null) : HSError() {
     init {
         super.type = ErrorType.SYNTAX
         super.name = "UnexpectedTokenError"
-        super.message = "Unexpected token '${token?.value}', expected a ${expectedTypes.toList().joinToString(" or ")}"
+        super.message = "Unexpected token '${token?.value}' of type ${token?.type}, expected a ${expectedValue ?: expectedTypes.toList().joinToString(" or ")}"
         super.line = token?.position?.first
         super.column = token?.position?.second
     }
