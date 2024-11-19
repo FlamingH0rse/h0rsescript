63d9a73c6f50c8fc8aa0a683c6c3d57771d63cdf
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-19 22:44:42 +0530

    Implemented Interpreter fully

--- src/nativeMain/kotlin/h0rsescript/Interpreter.kt
+++ src/nativeMain/kotlin/h0rsescript/Interpreter.kt
@@ -1,9 +1,9 @@
 package me.flaming.h0rsescript
 
-import me.flaming.h0rsescript.SyntaxTrees.FunctionCallNode
-import me.flaming.h0rsescript.SyntaxTrees.FunctionDefNode
-import me.flaming.h0rsescript.SyntaxTrees.IdentifierNode
-import me.flaming.h0rsescript.SyntaxTrees.LiteralNode
+import me.flaming.h0rsescript.syntax.FunctionCallNode
+import me.flaming.h0rsescript.syntax.FunctionDefNode
+import me.flaming.h0rsescript.syntax.IdentifierNode
+import me.flaming.h0rsescript.syntax.LiteralNode
 import me.flaming.h0rsescript.error.IllegalAssignmentError
 import me.flaming.h0rsescript.error.ReferenceError
 import me.flaming.h0rsescript.hs.MethodHandler
@@ -12,26 +12,37 @@ import me.flaming.h0rsescript.syntax.AssignmentNode
 import me.flaming.h0rsescript.tokens.TokenType
 import me.flaming.h0rsescript.tokens.Tokenizer
 
-class Interpreter(private val rawContent: String, private val options: Map<String, List<String>> = mapOf()) {
+class Interpreter(
+    private val rawContent: String,
+    private val options: Map<String, List<String>>,
+    private val flags: List<String>,
+    private val programArgs: List<String>
+) {
+
     // Variable class
     data class Variable(val name: String, val type: VariableType, var value: Any?)
     enum class VariableType { VARIABLE, CONSTANT, FUNCTION }
 
-    val variables = mutableListOf<Variable>()
 
-    val scopes = mutableListOf<MutableList<Variable>>()
+    private val scopes = mutableListOf<MutableList<Variable>>()
 
     fun run() {
+        // Tokenize the raw code
         var tokens = Tokenizer.tokenize(rawContent)
+
         // Remove whitespaces and comments
         tokens = tokens.filter { t -> t.type != TokenType.WHITESPACE && t.type != TokenType.COMMENT }.toMutableList()
 
         println(tokens.map { t -> t.value })
 
+        // Parse all tokens to ASTNode's
         val nodes = Parser.parse(tokens, options["parser-options"] ?: listOf())
         println(nodes)
 
+        // Execute the program
+
         // Adds Global scope to scopes
+        val variables = mutableListOf<Variable>()
         scopes.add(variables)
         nodes.forEach { evaluateNode(it) }
     }
@@ -43,67 +54,73 @@ class Interpreter(private val rawContent: String, private val options: Map<Strin
             is IdentifierNode -> {
                 val ref = node.name
 
-                // Checks starting from innermost scope, then upwards to global scope (variables)
-                for (scope in scopes.reversed()) {
-                    val foundVar = scope.firstOrNull() { v -> v.name == ref }
-                    if (foundVar != null) return foundVar.value
-                }
+                // Pre-defined identifiers
+                if (ref == "null") return null
+
+                // Gets variable from last available scope
+                val foundVar = getFromLastScope(ref)
+                if (foundVar != null) return foundVar.value
 
                 // Throw reference error if not found
                 ErrorHandler.report(ReferenceError(ref))
             }
 
+            // Throws IllegalAssignmentError
             is AssignmentNode -> {
                 println("Assigning...")
                 val variableName = node.name
                 val variableValue = evaluateNode(node.value)
+
                 val type = when (node.assignmentType) {
                     AssignmentNode.AssignmentType.CONSTANT -> VariableType.CONSTANT
                     AssignmentNode.AssignmentType.VARIABLE -> VariableType.VARIABLE
-                    AssignmentNode.AssignmentType.EDIT -> null
+                    else -> null
                 }
 
+                val error = IllegalAssignmentError(variableName)
 
+                // Edits a variable from last scope
                 if (node.assignmentType == AssignmentNode.AssignmentType.EDIT) {
-                    // Checks starting from innermost scope, then upwards to global scope (variables)
-                    // Then edits the value, unless it's a constant or a function
-                    for (scope in scopes.reversed()) {
-                        val foundVar = scope.firstOrNull() { v -> v.name == variableName }
-                        if (foundVar != null) {
-                            if (foundVar.type == VariableType.CONSTANT || foundVar.type == VariableType.FUNCTION) {
-                                val error = IllegalAssignmentError(variableName)
-                                error.message += when (foundVar.type) {
-                                    VariableType.CONSTANT -> "\nCannot edit a constant"
-                                    else -> "\nThat variable is already a function"
-                                }
-                                // Throw IllegalAssignmentError
-                                ErrorHandler.report(error)
-                            }
+                    // Gets variable from last available scope
+                    val foundVar = getFromLastScope(variableName)
 
-                            foundVar.value = variableValue
+                    if (foundVar != null) {
+                        // Throws IllegalAssignmentError if assignment to constant or function
+                        if (foundVar.type == VariableType.CONSTANT || foundVar.type == VariableType.FUNCTION) {
+                            error.message += when (foundVar.type) {
+                                VariableType.CONSTANT -> "\nCannot edit a constant"
+                                else -> "\nThat variable is already a function"
+                            }
+                            ErrorHandler.report(error)
                         }
+
+                        // Edits the variable's value
+                        foundVar.value = variableValue
                     }
+                    // Throws IllegalAssignmentError if variable doesn't exist
+                    else ErrorHandler.report(error)
                 }
+
                 // Creates Variable in current scope, unless already exists
                 else {
-                    val foundVar = scopes.last().firstOrNull() { v -> v.name == variableName }
+                    val foundVar = getFromCurrentScope(variableName)
+                    // Throw IllegalAssignmentError if variable already exists in current scope
                     if (foundVar != null) {
-                        // Throw IllegalAssignmentError
-                        val error = IllegalAssignmentError(variableName)
                         error.message += "\nA variable with that name already exists"
                         ErrorHandler.report(error)
                     }
 
-                    scopes.last().add(Variable(variableName, type!!, variableValue))
+                    currentScope().add(Variable(variableName, type!!, variableValue))
                 }
 
-                println(variables)
-
+                println(scopes)
             }
 
             is FunctionCallNode -> {
                 val functionName = node.name
                 val arguments = node.arguments.map { evaluateNode(it) }
+
+                val error = ReferenceError(functionName)
 
                 // Check for pre-existing methods
                 if (MethodHandler.exists(functionName)) {
@@ -112,46 +129,47 @@ class Interpreter(private val rawContent: String, private val options: Map<Strin
                 }
                 else {
                     // Check user-defined functions
-                    for (scope in scopes.reversed()) {
-                        val foundFunction = scope.firstOrNull() { v -> v.name == functionName }
-                        if (foundFunction == null) continue
-                        if (foundFunction.type == VariableType.FUNCTION) {
-                            // Create a local scope
-                            scopes.add(mutableListOf())
+                    val foundFunction = getFromLastScope(functionName)
 
-                            // Execute the function
-                            val options = (foundFunction.value as Pair<*, *>).first as Map<*, *>
-                            val body = (foundFunction.value as Pair<*, *>).second as List<*>
+                    // Throw ReferenceError if not found
+                    if (foundFunction == null) ErrorHandler.report(error)
 
-                            body.forEach { evaluateNode(it as ASTNode) }
-
-                            // Remove local scope after execution
-                            scopes.removeLast()
-
-                            // Return the output of the function
-                            return null
-                        } else {
-                            // Throw reference error if non-runnable function
-                            val error = ReferenceError(functionName)
-                            error.message += "\n'$functionName' is not a valid function name"
-                            ErrorHandler.report(error)
-                        }
+                    // Throw ReferenceError if non-runnable function
+                    if (foundFunction.type != VariableType.FUNCTION) {
+                        error.message += "\n'$functionName' is not a valid function name"
+                        ErrorHandler.report(error)
                     }
 
-                    // Throw reference error if not found
-                    ErrorHandler.report(ReferenceError(functionName))
+                    // Get function options
+                    val options = (foundFunction.value as Pair<*, *>).first as Map<*, *>
+                    val body = (foundFunction.value as Pair<*, *>).second as List<*>
+
+                    // Create a local scope
+                    val parameters = options["parameters"] as List<*>? ?: listOf<String>()
+                    val functionScope = parameters.zip(arguments) { k, v -> Variable(k as String, VariableType.VARIABLE, v)}
+                    scopes.add(functionScope.toMutableList())
+
+                    // Execute the function
+                    body.forEach { evaluateNode(it as ASTNode) }
+
+                    // Remove local scope after execution
+                    scopes.removeLast()
+
+                    // Return the output of the function
+                    return null
                 }
             }
 
+            // Throws IllegalAssignmentError
             is FunctionDefNode -> {
                 val functionName = node.name
                 val options = node.options
                 val body = node.body
 
                 // Check if already exists
-                val foundVar = scopes.last().firstOrNull() { v -> v.name == functionName }
+                val foundVar = getFromCurrentScope(functionName)
                 if (foundVar != null) {
-                    // Throw IllegalAssignmentError
+                    // Throw IllegalAssignmentError if already exists
                     val error = IllegalAssignmentError(functionName)
                     error.message += "\nA variable with that name already exists"
                     ErrorHandler.report(error)
@@ -161,6 +179,22 @@ class Interpreter(private val rawContent: String, private val options: Map<Strin
             else -> null
         }
     }
-    fun globalScope(): MutableList<Variable> = scopes[0]
-    fun currentScope(): MutableList<Variable> = scopes.last()
+
+    // Gets from the last available scope (checks all scopes, until it finds it)
+    // Used to check during modification or function call of an existing variable
+    private fun getFromLastScope(name: String): Variable? {
+        // Checks starting from innermost scope, then upwards to global scope (variables)
+        for (scope in scopes.reversed()) {
+            val foundVar = scope.firstOrNull() { v -> v.name == name }
+            if (foundVar != null) return foundVar
+        }
+        return null
+    }
+
+    // Gets from the current scope (scopes.last())
+    // Used to check during creation of a new variable
+    private fun getFromCurrentScope(name: String): Variable? = scopes.last().firstOrNull() { v -> v.name == name }
+
+    private fun globalScope(): MutableList<Variable> = scopes[0]
+    private fun currentScope(): MutableList<Variable> = scopes.last()
 }
