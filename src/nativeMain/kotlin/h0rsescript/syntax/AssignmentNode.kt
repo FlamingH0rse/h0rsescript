7b472f0fba98217c0055482bc240979ad89b8239
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-19 14:52:58 +0530

    Added assignment types

--- src/nativeMain/kotlin/h0rsescript/syntax/AssignmentNode.kt
+++ src/nativeMain/kotlin/h0rsescript/syntax/AssignmentNode.kt
@@ -2,5 +2,8 @@ package me.flaming.h0rsescript.syntax
 
 import me.flaming.h0rsescript.SyntaxTrees.IdentifierNode
 
-data class AssignmentNode(val name: String, val value: ASTNode, val locked: Boolean = false): ASTNode {
+data class AssignmentNode(val name: String, val value: ASTNode, val assignmentType: AssignmentType): ASTNode {
+    enum class AssignmentType {
+        VARIABLE, CONSTANT, EDIT
+    }
 }
