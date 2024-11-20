0bef42955e7a8159d6e377440a69f17129d665b7
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-20 21:24:20 +0530

    Added datatype check for method args

--- src/nativeMain/kotlin/h0rsescript/hs/Namespace.kt
+++ src/nativeMain/kotlin/h0rsescript/hs/Namespace.kt
@@ -1,8 +1,22 @@
 package me.flaming.h0rsescript.hs
 
+import me.flaming.h0rsescript.ErrorHandler
+import me.flaming.h0rsescript.error.TypeError
+
 abstract class Namespace {
     abstract val methods: Map<String, Method>
 
     fun hasMethod(name: String): Boolean = name in methods
-    fun executeMethod(name: String, arguments: List<HSType>): HSType = methods[name]?.execute(arguments) ?: HSType.NULL()
+    fun executeMethod(name: String, arguments: List<HSType>): HSType {
+        val method = methods[name]!!
+        val parametersMap = method.parameterTypes.zip(arguments)
+        // Check if parameters are the same type
+        for ((type, parameter) in parametersMap) {
+            if (!type.isInstance(parameter)) {
+                // Throw TypeError
+                ErrorHandler.report(TypeError(name, parameter::class, type))
+            }
+        }
+        return method.execute(arguments) ?: HSType.NULL()
+    }
 }
