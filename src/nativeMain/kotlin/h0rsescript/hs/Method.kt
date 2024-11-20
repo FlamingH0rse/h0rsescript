987142d6f8dc58f8df6608590613fe720a602033
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-20 21:25:39 +0530

    Changed Method class to return nullable type, but is replaced by non-nullable type HSType.NULL() in Namespace.executeMethod

--- src/nativeMain/kotlin/h0rsescript/hs/Method.kt
+++ src/nativeMain/kotlin/h0rsescript/hs/Method.kt
@@ -4,6 +4,6 @@ import me.flaming.h0rsescript.ErrorHandler
 import me.flaming.h0rsescript.error.TypeError
 import kotlin.reflect.KClass
 
-class Method(vararg val parameterTypes: KClass<out HSType>, val runnable: (List<HSType>) -> HSType) {
-    fun execute(arguments: List<HSType>): HSType = runnable.invoke(arguments)
+class Method(vararg val parameterTypes: KClass<out HSType>, val runnable: (List<HSType>) -> HSType?) {
+    fun execute(arguments: List<HSType>): HSType? = runnable.invoke(arguments)
 }
