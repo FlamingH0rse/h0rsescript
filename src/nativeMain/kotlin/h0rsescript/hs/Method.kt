d61acef4c46a71cf1091a6ba10839e7559898483
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-20 20:45:22 +0530

    Added Method class

---
+++ src/nativeMain/kotlin/h0rsescript/hs/Method.kt
@@ -0,0 +1,9 @@
+package me.flaming.h0rsescript.hs
+
+import me.flaming.h0rsescript.ErrorHandler
+import me.flaming.h0rsescript.error.TypeError
+import kotlin.reflect.KClass
+
+class Method(vararg val parameterTypes: KClass<out HSType>, val runnable: (List<HSType>) -> HSType) {
+    fun execute(arguments: List<HSType>): HSType = runnable.invoke(arguments)
+}
