775df7f0fa6c9eef8c309d30bcd69253381c32d0
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-11-21 00:38:40 +0530

    Changed parameter type of data method

--- src/nativeMain/kotlin/h0rsescript/hs/HSNamespace.kt
+++ src/nativeMain/kotlin/h0rsescript/hs/HSNamespace.kt
@@ -2,6 +2,6 @@ package me.flaming.h0rsescript.hs
 
 object HSNamespace : Namespace() {
     override val methods: Map<String, Method> = mapOf(
-        "data" to Method (HSType.STR::class, runnable = { args -> args[0] })
+        "data" to Method (HSType::class, runnable = { args -> args[0] })
     )
 }
