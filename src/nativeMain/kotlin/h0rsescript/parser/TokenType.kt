180a134cc7865889b427b73fcb2196dbdc616f70
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2024-12-29 16:13:05 +0530

    Removed TokenType WHITESPACE

--- src/nativeMain/kotlin/h0rsescript/parser/TokenType.kt
+++ src/nativeMain/kotlin/h0rsescript/parser/TokenType.kt
@@ -3,17 +3,19 @@ package me.flaming.h0rsescript.parser
 enum class TokenType {
     IDENTIFIER,    // Variable or function names
     QUALIFIED_IDENTIFIER, // Function signatures like math.add
-    KEYWORD,       // Keywords like $def, $end, etc.
+    KEYWORD,       // Keywords like $define, $end, etc.
+
     ASSIGNMENT_OPERATOR,        // Symbols like ->, <->, etc.
+
     STRING,        // String literals
     NUMBER,        // Numeric literals
-    BOOLEAN,       // true/false/TRUE/FALSE
+    BOOLEAN,       // TRUE/FALSE
+
     OPEN_BRACKET,  // [
     CLOSE_BRACKET, // ]
     OPEN_CURLY,    // {
     CLOSE_CURLY,   // }
     COMMA,         // ,
-    WHITESPACE,    // Spaces, tabs, newlines etc.
-//    NEWLINE,       // Newlines
+
     COMMENT,       // Comments
 }
