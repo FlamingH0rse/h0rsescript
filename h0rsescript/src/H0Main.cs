f8ff0fe1cfd4d9d4c927070d7481c776a9a057fe
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2026-06-15 23:25:34 +0530

    Init H0Main class, Implemented direct file running, TODO CLI

---
+++ h0rsescript/src/H0Main.cs
@@ -0,0 +1,39 @@
+﻿using h0rsescript.parser;
+
+namespace h0rsescript
+{
+    class H0Main
+    {
+        const string LANG_NAME = "h0rsescript";
+        const string VERSION = "0.0.1";
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Welcome to {LANG_NAME} v{VERSION}");
+                Console.WriteLine("Type h0 <file_name> to run it.");
+                
+                while (true)
+                {
+                    Console.Write(">> ");
+                    var command = Console.ReadLine();
+                    Console.WriteLine($"Running {command}...");
+                    // Run interpreter
+
+                }
+            }
+            else
+            {
+                if(!File.Exists(args[0]))
+                {
+                    Console.WriteLine("File no exist");
+                    return;
+                }
+                string src = File.ReadAllText(args[0]);
+                var tokens = Tokenizer.Tokenize(src);
+
+                tokens.ForEach(t => Console.WriteLine(t));
+            }
+        }
+    }
+}
